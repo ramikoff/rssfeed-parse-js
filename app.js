0bd1aa0f7325e7ff31bac45b5a2330fe14ac1be6
Ramil Novruzov <75280600+ramikoff@users.noreply.github.com>
2022-09-05 16:53:53 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const RSS_URL = `https://dev98.de/feed/`;
+
+fetch(RSS_URL)
+  .then((response) => response.text())
+  .then((str) => new window.DOMParser().parseFromString(str, 'text/xml'))
+  .then((data) => {
+    console.log(data);
+    const items = data.querySelectorAll('item');
+    let html = ``;
+    items.forEach((el) => {
+      html += `
+        <article>
+          <h2>
+            <a href="${
+              el.querySelector('link').innerHTML
+            }" target="_blank" rel="noopener">
+              ${el.querySelector('title').innerHTML}
+            </a>
+          </h2>
+        </article>
+      `;
+    });
+    document.body.insertAdjacentHTML('beforeend', html);
+  });
